9bf183469bb40c6f6a2beffa7e674c3647fd8ae0
Max <mmaksimm921@gmail.com>
2019-05-16 09:11:54 +0300

    read from file

--- Lab4/Lab4/image.h
+++ Lab4/Lab4/image.h
@@ -1,1 +1,25 @@
-#pragma once
+#include "BMPHEAD.h"
+#include "PIXELDATA.h"
+#include <string>
+using namespace std;
+
+class image {
+protected:
+	BMPHEAD info;
+	PIXELDATA **array;
+};
+
+class readImage:private image{
+public:
+	image read(const char*);
+};
+
+class resizeImage :private image {
+public:
+	image resize(image&, double);
+};
+
+class saveImage :private image {
+public:
+	void save(image&, string);
+};
